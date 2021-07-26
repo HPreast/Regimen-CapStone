54292d2679495a5f0b67bea8448f70615833e1cc
HPreast <preasths1@gmail.com>
2021-07-26 16:02:21 -0500

    Implemented fetch calls to return arm exercises and logic to show/hide the list of respective exercises

---
+++ Regimen/client/regimen/src/components/armsExerciseCard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const ArmsExerciseCard = ({ exercise }) => {
+    return (
+        <>
+            <p>{exercise.name}</p>
+        </>
+    )
+}
--- Regimen/client/regimen/src/modules/exerciseManager.js
+++ Regimen/client/regimen/src/modules/exerciseManager.js
@@ -1,7 +1,27 @@
 const baseUrl = "https://wger.de/api/v2"
 
-export const getAbsExercises = () => {
-    return fetch(`${baseUrl}/exerciseinfo/?language=2&category=10`, {
+export const GetAbsExercises = () => {
+    return fetch(`${baseUrl}/exercise/?category=10&language=2`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        },
+    })
+        .then(res => res.json())
+}
+
+export const GetArmExercises = () => {
+    return fetch(`${baseUrl}/exercise/?category=8&language=2`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        },
+    })
+        .then(res => res.json())
+}
+
+export const GetBackExercises = () => {
+    return fetch(`${baseUrl}/exercise/?language=2&category=12`, {
         method: "GET",
         headers: {
             "Content-Type": "application/json"
