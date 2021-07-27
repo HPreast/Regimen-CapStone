4496ca7bdbc41997525ce32964aaf4e8094567c6
HPreast <preasths1@gmail.com>
2021-07-27 09:34:58 -0500

    Implemented fetch calls and jsx to display all exercises grouped by category to the exercise list

--- Regimen/client/regimen/src/components/armsExerciseCard.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const ArmsExerciseCard = ({ exercise }) => {
-    return (
-        <>
-            <p>{exercise.name}</p>
-        </>
-    )
-}
--- Regimen/client/regimen/src/components/backExerciseCard.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const BackExerciseCard = ({ exercise }) => {
-    return (
-        <>
-            <p>{exercise.name}</p>
-        </>
-    )
-}
